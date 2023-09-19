5a92a42942e3116d75d10915c3fc4ac39d9cb957
ViniKrieger <viniheiko@gmail.com>
2023-09-19 09:49:13 -0300

    corrigindo a data do evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 
     AOS.init();
 
-    const dataDoEvento = new Date("Sept 12, 2023 19:00:00");
+    const dataDoEvento = new Date("Dec 12, 2023 19:00:00");
     const timeStampDoEvento = dataDoEvento.getTime();
 
     const contaAsHoras = setInterval(function() {
